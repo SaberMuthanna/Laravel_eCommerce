e9c8198e988f4b32e65c43d14abf083607247827
Saber Muthanna <34119145+SaberMuthanna@users.noreply.github.com>
2020-09-10 19:58:30 +0300

    Upload project

---
+++ resources/views/index.blade.php
@@ -0,0 +1,236 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <title>TheSaaS - Product list</title>
+    <!-- Styles -->
+    <link href="{{asset('assets/css/core.min.css')}}" rel="stylesheet">
+    <link href="{{asset('assets/css/thesaas.min.css')}}" rel="stylesheet">
+    <link href=" {{asset('assets/css/style.css')}}" rel="stylesheet">
+    <!-- Favicons -->
+  <link rel="apple-touch-icon" href=" {{asset('assets/img/apple-touch-icon.png')}}">
+    <link rel="icon" href=" {{asset('assets/img/favicon.png')}}">
+  </head>
+  <body>
+    <!-- Topbar -->
+    <nav class="topbar topbar-inverse topbar-expand-md topbar-sticky">
+      <div class="container"> 
+        <div class="topbar-left">
+          <button class="topbar-toggler">&#9776;</button>
+          <a class="topbar-brand" href="index">
+            <img class="logo-default" src=" {{asset('assets/img/logo.png')}}" alt="logo">
+            <img class="logo-inverse" src="{{asset('assets/img/logo-light.png')}}" alt="logo">
+          </a>
+        </div>
+        <div class="topbar-right">
+          <ul class="topbar-nav nav">
+            <li class="nav-item"><a class="nav-link" href="index">Home</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <!-- END Topbar -->
+    <!-- Header -->
+    <header class="header header-inverse bg-fixed" style="background-image: url(assets/img/bg-gift.jpg)" data-overlay="8">
+      <div class="container text-center">
+
+        <div class="row">
+          <div class="col-12 col-lg-8 offset-lg-2">
+
+            <h1>The Store</h1>
+            <p class="fs-20 opacity-70">You can find a list of our product in this page. We'll deliver your order in less than two days. Try it yourself!</p>
+
+          </div>
+        </div>
+
+      </div>
+    </header>
+    <!-- END Header -->
+    <!-- Main container -->
+    <main class="main-content">
+      <!--
+      |‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒
+      | Product list
+      |‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒
+      !-->
+      <section class="section">
+        <div class="container">
+          <div class="row gap-y">
+            <div class="col-12 col-md-6 col-xl-4">
+              <a class="shop-item" href="shop-single.html">
+                <div class="item-details">
+                  <div>
+                    <h5>Apple Watch 2</h5>
+                    <p>Superior Sports Watch</p>
+                  </div>
+
+                  <div class="item-price"><span class="unit">$</span>399</div>
+                </div>
+              <img src=" {{asset('assets/img/product-1.png')}}" alt="product">
+              </a>
+            </div>
+            <div class="col-12 col-md-6 col-xl-4">
+              <a class="shop-item" href="shop-single.html">
+                <div class="item-details">
+                  <div>
+                    <h5>Mac Book</h5>
+                    <p>The incredibly thin MacBook</p>
+                  </div>
+
+                  <div class="item-price"><span class="unit">$</span>1,299</div>
+                </div>
+              <img src="{{asset('assets/img/product-2.png')}}" alt="product">
+              </a>
+            </div>
+            <div class="col-12 col-md-6 col-xl-4">
+              <a class="shop-item" href="shop-single.html">
+                <div class="item-details">
+                  <div>
+                    <h5>Surface Pro 4</h5>
+                    <p>The Most Versatile Surface Yet</p>
+                  </div>
+
+                  <div class="item-price"><span class="unit">$</span>649</div>
+                </div>
+              <img src="{{asset('assets/img/product-3.png')}}" alt="product">
+              </a>
+            </div>
+
+            <div class="col-12 col-md-6 col-xl-4">
+              <a class="shop-item" href="shop-single.html">
+                <div class="item-details">
+                  <div>
+                    <h5>Galaxy Note 5</h5>
+                    <p>The most powerful Note to date</p>
+                  </div>
+
+                  <div class="item-price"><span class="unit">$</span>339</div>
+                </div>
+              <img src=" {{asset('assets/img/product-4.png')}}" alt="product">
+              </a>
+            </div>
+            <div class="col-12 col-md-6 col-xl-4">
+              <a class="shop-item" href="shop-single.html">
+                <div class="item-details">
+                  <div>
+                    <h5>iMac Retina</h5>
+                    <p>The vision is brighter than ever</p>
+                  </div>
+                  <div class="item-price"><span class="unit">$</span>1,499</div>
+                </div>
+              <img src=" {{asset('assets/img/product-5.png')}}" alt="product">
+              </a>
+            </div>
+            <div class="col-12 col-md-6 col-xl-4">
+              <a class="shop-item" href="shop-single.html">
+                <div class="item-details">
+                  <div>
+                    <h5>Moto 360</h5>
+                    <p>Android Wear SmartWatch</p>
+                  </div>
+
+                  <div class="item-price"><span class="unit">$</span>199</div>
+                </div>
+              <img src="{{asset('assets/img/product-6.png')}}" alt="product">
+              </a>
+            </div>
+
+
+            <div class="col-12 col-md-6 col-xl-4">
+              <a class="shop-item" href="shop-single.html">
+                <div class="item-details">
+                  <div>
+                    <h5>Mac Book Pro</h5>
+                    <p>A touch of genius</p>
+                  </div>
+
+                  <div class="item-price"><span class="unit">$</span>2,799</div>
+                </div>
+              <img src="{{asset('assets/img/product-7.png')}}" alt="product">
+              </a>
+            </div>
+
+
+            <div class="col-12 col-md-6 col-xl-4">
+              <a class="shop-item" href="shop-single.html">
+                <div class="item-details">
+                  <div>
+                    <h5>Nokia 220</h5>
+                    <p>Dual SIM mobile</p>
+                  </div>
+
+                  <div class="item-price"><span class="unit">$</span>148</div>
+                </div>
+              <img src="{{asset('assets/img/product-8.png')}}" alt="product">
+              </a>
+            </div>
+
+
+            <div class="col-12 col-md-6 col-xl-4">
+              <a class="shop-item" href="shop-single.html">
+                <div class="item-details">
+                  <div>
+                    <h5>iPhone 7 Plus</h5>
+                    <p>Now with all-new Portrait mode</p>
+                  </div>
+
+                  <div class="item-price"><span class="unit">$</span>769</div>
+                </div>
+              <img src="{{asset('assets/img/product-9.png')}}" alt="product">
+              </a>
+            </div>
+          </div>
+
+        </div>
+      </section>
+    </main>
+    <!-- END Main container -->
+    <!-- Footer -->
+    <footer class="site-footer">
+      <div class="container">
+        <div class="row gap-y align-items-center">
+          <div class="col-12 col-lg-3">
+            <p class="text-center text-lg-left">
+            <a href="index"><img src="{{asset('assets/img/logo.png')}}" alt="logo"></a>
+            </p>
+          </div>
+
+          <div class="col-12 col-lg-6">
+            <ul class="nav nav-primary nav-hero">
+              <li class="nav-item">
+                <a class="nav-link" href="index">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="index">Features</a>
+              </li>
+              <li class="nav-item hidden-sm-down">
+                <a class="nav-link" href="index">Pricing</a>
+              </li>
+              <li class="nav-item hidden-sm-down">
+                <a class="nav-link" href="index">Contact</a>
+              </li>
+            </ul>
+          </div>
+
+          <div class="col-12 col-lg-3">
+            <div class="social text-center text-lg-right">
+              <a class="social-facebook" href="https://www.facebook.com/thethemeio"><i class="fa fa-facebook"></i></a>
+              <a class="social-twitter" href="https://twitter.com/thethemeio"><i class="fa fa-twitter"></i></a>
+              <a class="social-instagram" href="https://www.instagram.com/thethemeio/"><i class="fa fa-instagram"></i></a>
+              <a class="social-dribbble" href="https://dribbble.com/thethemeio"><i class="fa fa-dribbble"></i></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+    <!-- END Footer -->
+    <!-- Scripts -->
+  <script src="{{asset('assets/js/core.min.js')}}"></script>
+  <script src="{{asset('assets/js/thesaas.min.js')}}"></script>
+  <script src="{{asset('assets/js/script.js')}}"></script>
+
+  </body>
+</html>
